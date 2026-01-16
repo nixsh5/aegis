db1c91ff5472c999fe9c6724a9156b97b499526e
Nish <nishdeshmukh2004@gmail.com>
2026-01-16 22:29:27 +0530

    Rust File

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::post,
+    Router,
+    http::{StatusCode, Request},
+    response::IntoResponse,
+    body::Body
+};
+use std::process::Command;
+use tower_http::validate_request::ValidateRequestHeaderLayer;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/lock", post(lock_handler))
+        .layer(ValidateRequestHeaderLayer::custom(|request: &mut Request<Body>| {
+            let api_key = request
+                .headers()
+                .get("X Api Key")
+                .and_then(|value| value.to_str().ok());
+
+            if api_key == Some("MY_SUPER_SECRET_API_KEY") {
+                Ok(())
+            } else {
+                Err(StatusCode::UNAUTHORIZED.into_response())
+            }
+        }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    println!("Server listening on port 8080...");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn lock_handler() -> impl IntoResponse {
+    #[cfg(target_os = "linux")]
+    let _ = Command::new("hyprlock").arg("lock").spawn();
+
+    StatusCode::OK
 }
